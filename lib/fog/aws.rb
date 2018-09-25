e2c48399e30ca658ecea77d176db0f7f803027f8
Daniel Zarate <dzarate@webdox.cl>
2018-09-25 18:23:23 -0300

    done

--- lib/fog/aws.rb
+++ lib/fog/aws.rb
@@ -229,10 +229,12 @@ module Fog
     end
 
     def self.regions
-      @regions ||= ['ap-northeast-1', 'ap-northeast-2', 'ap-southeast-1', 'ap-southeast-2', 'eu-central-1', 'ca-central-1', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'sa-east-1', 'cn-north-1', 'us-gov-west-1', 'ap-south-1']
+      binding.pry
+      @regions ||= ['sa-chile-1']
     end
 
     def self.validate_region!(region, host=nil)
+      binding.pry
       if (!host || host.end_with?('.amazonaws.com')) && !regions.include?(region)
         raise ArgumentError, "Unknown region: #{region.inspect}"
       end
